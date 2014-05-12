dd53e9bde0d1edc5a1eabe826c5ae1f8e73ce51e
gunnar.almas <gunnar.almas@gmail.com>
2014-05-12 21:53:53 +0200

    test fra ws

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,26 @@
 'use strict';
 
 angular.module('testApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,$firebase) {
+
+		var peopleRef = new Firebase("https://gatestdb.firebaseio.com/people1");
+		$scope.people = $firebase(peopleRef);
+		$scope.addPerson = function() {
+			// AngularFire $add method
+			$scope.people.$add($scope.newPerson);
+
+			//or add a new person manually
+			//peopleRef.update({name: 'Alex', age: 35});
+
+			$scope.newPerson = "";
+		}
+
+		$scope.listPersons = function() {
+			var keys = $scope.people.$getIndex();
+			keys.forEach(function(key, i) {
+				console.log(i, $scope.people[key]); // Prints items in order they appear in Firebase.
+			});
+		}
 
     $scope.names = [
       'HTML5 Boilerplate',
