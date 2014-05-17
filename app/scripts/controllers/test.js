71252ea88db1793af36e986d8c942d3ce49bbfea
gunnar.almas <gunnar.almas@gmail.com>
2014-05-17 16:39:11 +0200

    firebase

---
+++ app/scripts/controllers/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('testApp')
+  .controller('TestCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
